15e987801b41eab966d70c7ee278bc332b1c473b
Samiurrahman Shaikh <Samiurrahman.Shaikh@morningstar.com>
2019-05-23 16:57:54 +0530

    initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+    { path: '', loadChildren: './home/home.module#HomeModule' },
+    { path: 'user/:username', loadChildren: './user/user.module#UserModule'},
+    { path: 'policy', component: PolicyComponent },
+    { path: 'qrcode/:username', component: QrcodeComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '/notfound' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
+
